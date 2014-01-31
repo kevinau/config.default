7535db69ddc138c48f0ccecb3224493b9ab8c005
Kevin <kholloway@geckosoftware.co.uk>
2014-01-31 20:46:14 +0000

    Initial commit

---
+++ org.gyfor.branding/src/org/gyfor/branding/IBranding.java
@@ -0,0 +1,19 @@
+package org.gyfor.branding;
+
+public interface IBranding {
+
+  public String getName ();
+  
+  public String getURL ();
+
+  public String getLogoText();
+
+  public String getLogoURL();
+  
+  public String getFaviconURL();
+  
+  public String getStyleCss();
+  
+  public boolean isScreenshotTitle();
+  
+}
---
+++ org.gyfor.branding/src/org/gyfor/branding/ScreenshotBranding.java
@@ -0,0 +1,10 @@
+package org.gyfor.branding;
+
+public class ScreenshotBranding extends NoBranding {
+
+  @Override
+  public boolean isScreenshotTitle () {
+    return true;
+  }
+  
+}
